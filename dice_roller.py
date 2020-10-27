96b11557c3962a27f1b8e0a9933c68195f7a5fc9
Anshumanpandey <anshumanp70@gmail.com>
2020-10-27 10:41:49 +0530

    version 1

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,9 @@
+import random
 def main():
-  #print('You rolled a die')
+  print('You rolled a die')
+  n = random.randint(1,6)
+  print('Congratulations..! you got ' + str(n))
+  print('Thank you')
 
 if __name__== "__main__":
   main()
