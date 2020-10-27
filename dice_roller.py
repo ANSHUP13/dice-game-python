ca605125be8cb808ea946c2d4fe1ad299b9e86fc
Anshumanpandey <anshumanp70@gmail.com>
2020-10-27 11:00:49 +0530

    version 3

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,11 @@
 import random
 def main():
+  dicesides = int(input('enter the number of sides of dice- '))
   dice_to_roll = int(input('how many dice you want to roll?'))
   nsum = 0
   for i in range(dice_to_roll):
     print('You rolled dice no '+str(i+1))
-    n = random.randint(1,6)
+    n = random.randint(1,dicesides)
     print('Congratulations..! you got ' + str(n))
     nsum += n
   print('you got total of '+str(nsum))
