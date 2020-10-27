2b9025939baebd113ebc03a7acf8e194d577c0a0
Anshumanpandey <anshumanp70@gmail.com>
2020-10-27 10:53:36 +0530

    version 2

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,13 @@
 import random
 def main():
-  print('You rolled a die')
-  n = random.randint(1,6)
-  print('Congratulations..! you got ' + str(n))
+  dice_to_roll = int(input('how many dice you want to roll?'))
+  nsum = 0
+  for i in range(dice_to_roll):
+    print('You rolled dice no '+str(i+1))
+    n = random.randint(1,6)
+    print('Congratulations..! you got ' + str(n))
+    nsum += n
+  print('you got total of '+str(nsum))
   print('Thank you')
 
 if __name__== "__main__":
